257d008d08613b1db96649175fb2d08c46274d71
Nafis-Fahim50 <nafisf96@gmail.com>
2022-10-26 01:08:56 +0600

    added home page

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Home.css'
 
 
 const Home = () => {
 
     return (
-        <div>
-
+        <div className='my-5 text-center rounded shadow py-5 home-container'>
+            <h1 className='text-success fw-bold mb-4 title' >Learn Programming</h1>
+            <h5 className='text-warning fw-bold mb-4'>With the world's largest programming site.</h5>
+            <p className='text-dark mb-4'>Well organized and easy to understand Web building tutorials with lots of examples of how to use HTML, CSS, JavaScript, SQL, Python, PHP, Bootstrap, Java, C++, C# etc.</p>
+            <button className='btn btn-success'>
+                <Link className='text-decoration-none text-light' to='/courses'>Checkout Our All Courses</Link>
+            </button>
         </div>
     );
 };
