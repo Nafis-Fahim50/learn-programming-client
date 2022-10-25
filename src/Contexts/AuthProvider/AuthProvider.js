a2304ffce0332d289fe88e1e9ced7662be9caa65
Nafis-Fahim50 <nafisf96@gmail.com>
2022-10-25 12:28:40 +0600

    set login toggle options

--- src/Contexts/AuthProvider/AuthProvider.js
+++ src/Contexts/AuthProvider/AuthProvider.js
@@ -1,27 +1,45 @@
 import React, { createContext } from 'react';
-import {createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signInWithPopup, signOut} from 'firebase/auth'
+import {createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut} from 'firebase/auth'
 import app from '../../firebase/firebase.config';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 export const AuthContext = createContext();
 const auth = getAuth(app)
 
 const AuthProvider = ({children}) => {
-    const user = {displayName: 'nafis'}
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
 
     const providerLogin = (provider) =>{
+        setLoading(true)
         return signInWithPopup(auth,provider);
     }
     const createUser = (email, password) =>{
+        setLoading(true);
         return createUserWithEmailAndPassword(auth,email,password);
     }
 
     const signIn = (email, password) =>{
+        setLoading(true);
         return signInWithEmailAndPassword(auth, email, password);
     }
 
     const logout = () =>{
+        setLoading(true);
         return signOut(auth);
     }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser)=>{
+            setUser(currentUser);
+            setLoading(false);
+        });
+        return () =>{
+            unsubscribe();
+        }
+    }
+        ,[])
 
     const authInfo ={user,
         createUser,
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { useContext } from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, Image } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import { FaSignOutAlt } from 'react-icons/fa';
+import { FaSignOutAlt, FaUser } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../../../Contexts/AuthProvider/AuthProvider';
 import image from '../../../image/image.png'
 
 const Header = () => {
-    const { user,logout } = useContext(AuthContext);
+    const { user, logout } = useContext(AuthContext);
     return (
         <Navbar bg="light" expand="lg">
             <Container>
@@ -25,27 +25,35 @@ const Header = () => {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link><Link className='text-muted text-decoration-none' to='/courses'>Courses</Link></Nav.Link>
-                        <Nav.Link><Link className='text-muted text-decoration-none' to='/faq'>FAQ</Link></Nav.Link>
-                        <Nav.Link><Link className='text-muted text-decoration-none' to='/blog'>Blog</Link></Nav.Link>
-                        <Nav.Link><Link className='text-muted text-decoration-none' to='/signup'>Signup</Link></Nav.Link>
-                        <Nav.Link><Link className='text-muted text-decoration-none' to='/login'>Login</Link></Nav.Link>
+                        <Link className=' mt-1 text-muted text-decoration-none' to='/courses'>Courses</Link>
+                        <Link className=' mt-1 ms-2 text-muted text-decoration-none' to='/faq'>FAQ</Link>
+                        <Link className=' mt-1 ms-2 text-muted text-decoration-none' to='/blog'>Blog</Link>
                     </Nav>
                     <Nav>
-                        <Nav.Link>
+                        {
+                            user?.photoURL ?
+                                <Image style={{ height: '50px' }}
+                                    roundedCircle
+                                    src={user?.photoURL}>
+                                </Image>
+                                :
+                                <FaUser className='mt-1'></FaUser>
+                        }
+
+                        <>
                             {
                                 user?.uid ?
                                     <>
-                                        <span className='me-3'>{user?.displayName}</span>
+                                        <span className='mx-2 mt-2 fw-bold'>{user?.displayName}</span>
                                         <Button onClick={logout}><FaSignOutAlt></FaSignOutAlt> Logout</Button>
                                     </>
                                     :
-                                    <></>
+                                    <>
+                                        <Link className='text-decoration-none text-warning me-2 ms-2' to='/signup'>Signup</Link>
+                                        <Link className='text-decoration-none text-success me-2' to='/login'>Login</Link>
+                                    </>
                             }
-                        </Nav.Link>
-                        <Nav.Link eventKey={2} href="#memes">
-                            Dank memes
-                        </Nav.Link>
+                        </>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
