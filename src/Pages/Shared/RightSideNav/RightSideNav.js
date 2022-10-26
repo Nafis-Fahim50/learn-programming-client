f26cd69cdf055ecfe68016ee50d8bca3cae3ecb1
Nafis-Fahim50 <nafisf96@gmail.com>
2022-10-26 13:07:36 +0600

    added contract info

--- src/Pages/Shared/RightSideNav/RightSideNav.js
+++ src/Pages/Shared/RightSideNav/RightSideNav.js
@@ -1,27 +1,35 @@
 import React, { useEffect } from 'react';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import ListGroup from 'react-bootstrap/ListGroup';
+import {FaFacebook, FaTwitter, FaWhatsapp} from "react-icons/fa";
 
 const RightSideNav = () => {
     const [courses, setcourses] = useState([])
 
-    useEffect(()=>{
+    useEffect(() => {
         fetch('https://learn-programming-server-side.vercel.app/courses')
-        .then(res => res.json())
-        .then(data => setcourses(data))
-    },[])
+            .then(res => res.json())
+            .then(data => setcourses(data))
+    }, [])
 
     return (
         <div className='mt-3'>
-          <h4 className='text-success mb-3'>Quick Links</h4>
-          <div>
-            {
-                courses.map(course => <p
-                key={course.id}>
-                    <Link className='text-decoration-none text-dark fw-bold' to={`/course/${course.id}`}>Learn {course.name}</Link>
-                </p>)
-            }
-          </div>
+            <h4 className='text-success mb-3'>Quick Links</h4>
+            <div>
+                {
+                    courses.map(course => <p
+                        key={course.id}>
+                        <Link className='text-decoration-none text-dark fw-bold' to={`/course/${course.id}`}>Learn {course.name}</Link>
+                    </p>)
+                }
+            </div>
+            <h4 className='text-success my-2'>Contract Us</h4>
+            <ListGroup>
+                <ListGroup.Item className='mb-3 mt-2 text-primary'><FaFacebook /> Facebook</ListGroup.Item>
+                <ListGroup.Item className='mb-3 text-success'><FaWhatsapp /> WhatsApp</ListGroup.Item>
+                <ListGroup.Item className='mb-3 text-info'><FaTwitter /> Twitter</ListGroup.Item>
+            </ListGroup>
         </div>
     );
 };
