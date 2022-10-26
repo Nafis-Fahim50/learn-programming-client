874bbc70dcb975fbc747b868dc9927d0968be478
Nafis-Fahim50 <nafisf96@gmail.com>
2022-10-26 21:09:18 +0600

    downlaod course details pdf

--- src/Pages/CourseDetails/CourseDetails.js
+++ src/Pages/CourseDetails/CourseDetails.js
@@ -4,18 +4,23 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { FaDownload } from 'react-icons/fa';
 import './CourseDetails.css'
+import Pdf from "react-to-pdf";
 
+const ref = React.createRef();
 
 const CourseDetails = () => {
     const details = useLoaderData();
-    const { img_url, discription, title, name,id } = details;
+    const { img_url, discription, title, name, id } = details;
+
     return (
         <div>
             <div className='d-flex justify-content-between mt-3'>
                 <h3>{name}</h3>
-                <FaDownload className='save-icon'></FaDownload>
+                <Pdf targetRef={ref} filename={`${name}.pdf`}>
+                    {({ toPdf }) => <FaDownload onClick={toPdf} className='save-icon'></FaDownload>}
+                </Pdf>
             </div>
-            <Card>
+            <Card ref={ref}>
                 <Card.Img variant="top" src={img_url} />
                 <Card.Body>
                     <Card.Title>{title}</Card.Title>
